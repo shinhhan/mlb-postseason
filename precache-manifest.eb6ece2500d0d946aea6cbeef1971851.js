5ef73f38c2304564c957abf45a43acfb5443341c
Shin Han <shin.han@razorfish.com>
2019-05-15 13:21:49 -0400

    Updates

---
+++ precache-manifest.eb6ece2500d0d946aea6cbeef1971851.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9504467410da05efd986b897afbb676e",
+    "url": "/mlb-postseason/index.html"
+  },
+  {
+    "revision": "c9779771990f35ebfb00",
+    "url": "/mlb-postseason/static/css/main.2c55baee.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/mlb-postseason/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c9779771990f35ebfb00",
+    "url": "/mlb-postseason/static/js/main.c644cc54.chunk.js"
+  },
+  {
+    "revision": "8ca685fa49f9be3c1cd2",
+    "url": "/mlb-postseason/static/js/runtime~main.6731d02e.js"
+  },
+  {
+    "revision": "7b0ca749dbb23ac1b7411e1a6aa526d8",
+    "url": "/mlb-postseason/static/media/benton-sans-cond-medium.7b0ca749.woff2"
+  },
+  {
+    "revision": "06cda30a3eea6fd9409d9ca48ea39553",
+    "url": "/mlb-postseason/static/media/icomoon.06cda30a.woff"
+  },
+  {
+    "revision": "2c97c23a31f1332a3d15db5b2641abb8",
+    "url": "/mlb-postseason/static/media/icomoon.2c97c23a.ttf"
+  },
+  {
+    "revision": "4bc3b9d5bf9b1ec8e1aea21f2d413e87",
+    "url": "/mlb-postseason/static/media/icomoon.4bc3b9d5.svg"
+  },
+  {
+    "revision": "ec443f1b0275abf44c06ca19f7fb5083",
+    "url": "/mlb-postseason/static/media/icomoon.ec443f1b.eot"
+  },
+  {
+    "revision": "636ec413377f09da989ee837ec64b7f4",
+    "url": "/mlb-postseason/static/media/logo.636ec413.svg"
+  }
+]);
